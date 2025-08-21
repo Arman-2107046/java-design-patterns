71ffda12b311a3b2d618fd49085c7a572274811f
Arman-2107046 <armanr.rafi@gmail.com>
2025-08-21 19:35:02 +0600

    Modify add method in master to create conflict

---
+++ repository/src/main/java/com/iluwatar/repository/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+
+
+    
+public int add(int a, int b) {
+    return a + b + 1; // change for conflict
+}
+
+
+
+    public int subtract(int a, int b) { return a - b; }
+    public int multiply(int a, int b) { return a * b; }
+    public int divide(int a, int b) { return a / b; }
+}
